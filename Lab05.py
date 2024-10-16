868f3299c23c7e96e9db554895a6aa433faf616f
Griffin Steinman <steinmag@tcd.ie>
2024-10-16 12:46:48 +0100

    changes

--- Lab05.py
+++ Lab05.py
@@ -25,8 +25,19 @@ from sklearn.model_selection import cross_val_score
 
 import subprocess
 
+
+import os
+import subprocess
+import pickle
+import tensorflow as tf
+from keras import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.callbacks import Callback
+from matplotlib.ticker import MaxNLocator
+
 # Define some useful functions
-class PlotLossAccuracy(keras.callbacks.Callback):
+class PlotLossAccuracy(Callback):
     def on_train_begin(self, logs={}):
         self.i = 0
         self.x = []
@@ -37,7 +48,6 @@ class PlotLossAccuracy(keras.callbacks.Callback):
         self.logs = []
 
     def on_epoch_end(self, epoch, logs={}):
-
         self.logs.append(logs)
         self.x.append(int(self.i))
         self.losses.append(logs.get('loss'))
@@ -47,9 +57,7 @@ class PlotLossAccuracy(keras.callbacks.Callback):
 
         self.i += 1
 
-        clear_output(wait=True)
         plt.figure(figsize=(16, 6))
-        plt.plot([1, 2])
         plt.subplot(121)
         plt.plot(self.x, self.losses, label="train loss")
         plt.plot(self.x, self.val_losses, label="validation loss")
@@ -66,137 +74,53 @@ class PlotLossAccuracy(keras.callbacks.Callback):
         plt.xlabel('epoch')
         plt.title('Model Accuracy')
         plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.show();
+        plt.show()
 
-def save_model_to_disk():
-    # save model and weights (don't change the filenames)
+def save_model_to_disk(model):
     model_json = model.to_json()
     with open("model.json", "w") as json_file:
         json_file.write(model_json)
-    # serialize weights to HDF5
     model.save_weights("model.h5")
     print("Saved model to model.json and weights to model.h5")
-    
-    
-    
-    
-    print('loading the dataset...')
-    
-subprocess.run([
-    "curl", "--create-dirs", 
-    "-o", "/home/tcd/data/cifar10-dataset.pkl", 
-    "https://tcddeeplearning.blob.core.windows.net/deeplearning202324/cifar10-dataset.pkl"
-])
 
-pkl_file = open('/home/tcd/data/cifar10-dataset.pkl', 'rb')
-dataset = pickle.load(pkl_file)
+# File path to the dataset
+dataset_path = "/home/tcd/data/cifar10-dataset.pkl"
 
-print('loaded.')
+# Check if the dataset already exists
+if not os.path.exists(dataset_path):
+    print("Dataset not found, downloading the dataset...")
+    subprocess.run([
+        "curl", "--create-dirs", 
+        "-o", dataset_path, 
+        "https://tcddeeplearning.blob.core.windows.net/deeplearning202324/cifar10-dataset.pkl"
+    ])
+else:
+    print("Dataset already exists. Skipping download.")
 
-print('let\'s look at some of the pictures and their ground truth labels:')
+# Load the dataset
+try:
+    with open(dataset_path, 'rb') as pkl_file:
+        dataset = pickle.load(pkl_file)
+    print("Dataset loaded successfully.")
+except pickle.UnpicklingError:
+    print("Error loading the dataset. The file might be corrupt.")
 
-plt.figure(figsize=(12,12))
-plt.plot([3, 3])
-
-X = dataset['X'].astype('float32')/255
+# Prepare the dataset
+X = dataset['X'].astype('float32') / 255
 Y = dataset['Y'].astype('float32')
 Y = tf.keras.utils.to_categorical(Y)
 
-for i in range(0,9):
-    # pictures are 32x32x3 (width=32, height=32, 3 colour channels)
+# Visualize some images
+plt.figure(figsize=(12, 12))
+for i in range(9):
     pic = X[i]
-
-    # Y[i] returns an array of zeros and with Y[i][classid] = 1
-    # for instance  Y[i] = [ 0 0 0 0 0 1 0 0 0 0] => classid=5
-    #          and  Y[i] = [ 1 0 0 0 0 0 0 0 0 0] => classid=0
-    # we can get the classid by using the argmax function on the vector Y[i]
     classid = Y[i].argmax(-1)
-
-    # getting back the name of the label for that classid
     classname = dataset['labels'][classid]
-    plt.subplot(3,3,i+1)
+    plt.subplot(3, 3, i + 1)
     plt.imshow(pic)
-    plt.title('label: {}'.format(classname))
+    plt.title(f'label: {classname}')
+plt.show()
 
 
 
-# let's split data between validation set and training set
-
-X_train, X_validation, Y_train, Y_validation = skl.model_selection.train_test_split(X, Y, test_size=.1)
-
-# The model below contains 2 hidden layers with 64 nodes each.
-# The activation functions for these 2 layers is the ReLU
-# The network ends with a 10 nodes layer with softmax activation
-# The first 2 hidden layers transform the original features into
-# a new feature vector of size 64.
-# The last layer essentially does the classification using multonomial regression
-# based on these new features.
-
-inputs = keras.layers.Input(shape=(32, 32, 3))
-
-
-x = inputs  # Don't flatten until after your 2D layers
-...
-# put here some convolutional layers;
-# see keras docs for Conv2D, MaxPool2D, etc.
-# Maybe stick in some Dropout as well?
-
-x = Conv2D(500, (3, 3), activation='relu', padding='same')(x)
-x = MaxPool2D((2, 2))(x)
-x = Conv2D(500, (3, 3), activation='relu', padding='same')(x)
-x = MaxPool2D((2, 2))(x)
-x = Conv2D(300, (3, 3), activation='relu', padding='same')(x)
-x = MaxPool2D((2, 2))(x)
-x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
-x = MaxPool2D((2, 2))(x)
-
-x = Dropout(0.25)(x)
-
-
-
-
-# The last layers of your model should look something like
-# this to be well-structured for the final classification:
-x = Flatten()(x)
-
-x = Dense(128, activation='relu')(x)
-x = Dropout(0.5)(x)
-
-
-
-predictions = Dense(10, activation='softmax')(x)  # There are 10 classes in this problem, hence the '10'.
-
-
-
-# Create the model.
-model = keras.models.Model(inputs=inputs, outputs=predictions)
-opt = keras.optimizers.legacy.SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-
-# Setup the optimisation strategy.
-model.compile(optimizer=opt,
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-# Display a summary.
-model.summary()
-
-# Keep things sane.
-if (model.count_params() > 4500000):
-    raise Exception("Your model is unecessarily complex, scale down!")
-
-
-
-# Note that you can evaluate this cell repeatedly to push the training of your model further.
-# You might want to reduce the value of 'num_epochs' if each evaluation starts to take too long.
-
-num_epochs = 40
-
-# Create an instance of our callback functions class, to plot our loss function and accuracy with each epoch.
-pltCallBack = PlotLossAccuracy()
-
-# Run the training.
-model.fit(X_train, Y_train,
-          batch_size=1024, epochs=num_epochs,
-          validation_data=(X_validation, Y_validation),
-          callbacks=[pltCallBack])
 
---
+++ lab5run.py
@@ -0,0 +1,82 @@
+from Lab05 import *
+
+
+# let's split data between validation set and training set
+
+X_train, X_validation, Y_train, Y_validation = skl.model_selection.train_test_split(X, Y, test_size=.1)
+
+# The model below contains 2 hidden layers with 64 nodes each.
+# The activation functions for these 2 layers is the ReLU
+# The network ends with a 10 nodes layer with softmax activation
+# The first 2 hidden layers transform the original features into
+# a new feature vector of size 64.
+# The last layer essentially does the classification using multonomial regression
+# based on these new features.
+
+inputs = keras.layers.Input(shape=(32, 32, 3))
+
+
+x = inputs  # Don't flatten until after your 2D layers
+...
+# put here some convolutional layers;
+# see keras docs for Conv2D, MaxPool2D, etc.
+# Maybe stick in some Dropout as well?
+
+x = Conv2D(500, (3, 3), activation='relu', padding='same')(x)
+x = MaxPool2D((2, 2))(x)
+x = Conv2D(500, (3, 3), activation='relu', padding='same')(x)
+x = MaxPool2D((2, 2))(x)
+x = Conv2D(300, (3, 3), activation='relu', padding='same')(x)
+x = MaxPool2D((2, 2))(x)
+x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+x = MaxPool2D((2, 2))(x)
+
+x = Dropout(0.25)(x)
+
+
+
+
+# The last layers of your model should look something like
+# this to be well-structured for the final classification:
+x = Flatten()(x)
+
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.5)(x)
+
+
+
+predictions = Dense(10, activation='softmax')(x)  # There are 10 classes in this problem, hence the '10'.
+
+
+
+# Create the model.
+model = keras.models.Model(inputs=inputs, outputs=predictions)
+opt = keras.optimizers.legacy.SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+
+# Setup the optimisation strategy.
+model.compile(optimizer=opt,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Display a summary.
+model.summary()
+
+# Keep things sane.
+if (model.count_params() > 4500000):
+    raise Exception("Your model is unecessarily complex, scale down!")
+
+
+
+# Note that you can evaluate this cell repeatedly to push the training of your model further.
+# You might want to reduce the value of 'num_epochs' if each evaluation starts to take too long.
+
+num_epochs = 40
+
+# Create an instance of our callback functions class, to plot our loss function and accuracy with each epoch.
+pltCallBack = PlotLossAccuracy()
+
+# Run the training.
+model.fit(X_train, Y_train,
+          batch_size=1024, epochs=num_epochs,
+          validation_data=(X_validation, Y_validation),
+          callbacks=[pltCallBack])
